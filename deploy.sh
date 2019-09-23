7c5c6622459d28e526bf4896e95356407ceb5cb1
娄通略 <1746299291@qq.com>
2019-09-23 23:53:48 +0800

    增加一键部署shell脚本

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+cd case01
+nohup node server &
+cd ../case02
+nohup node server &
+cd ../case03
+nohup node server &
+cd ../case04
+nohup node server &
+cd ../case05
+nohup node server &
+cd ../case06
+nohup node server &
+cd ../case07
+nohup node server &
+cd ../case08
+nohup node server &
+cd ../case09
+nohup node server &
